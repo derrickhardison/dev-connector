b5cc2421b69fcbd6536a74037d676288fcd9badb
dlhardi <derricklhardison@gmail.com>
2021-04-13 20:00:20 -0400

    Setup routes for testing via Postman

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
 const express = require("express");
+const connectDB = require("./config/db");
 const app = express();
+
+// connect database
+connectDB();
 
 const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
   res.send("API running");
 });
+
+// Define routes
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/post", require("./routes/api/post"));
+
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
 });
