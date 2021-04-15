b1c5fe40835d7079d20271eb1e00beb5978c7aee
dlhardi <derricklhardison@gmail.com>
2021-04-14 22:11:24 -0400

    Changed route to POST route. Set validation checks using express-validator, added model and now checking to see if user already present in database

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,12 +1,41 @@
 const express = require("express");
 const router = express.Router();
+const { check, validationResult } = require("express-validator/check");
 
-// @route   GET api/users
-// @desc    Test route
+const User = require("../../models/User");
+// @route   POST api/users
+// @desc    Register user
 // @access  Public
 //           - private routes require tokens
-router.get("/", (req, res) => {
-  res.send("user route");
-});
+
+router.post(
+  "/",
+  [
+    check("name", "Name is required").not().isEmpty(),
+    check("email", "Please include your email address").isEmail(),
+    check(
+      "password",
+      "Please enter a password with 6 or more characters"
+    ).isLength({ min: 6 }),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const { name, email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email });
+      if (user) {
+        res.status(400).json({ errors: [{ msg: "User already exists" }] });
+      }
+      res.send("user route");
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server error");
+    }
+  }
+);
 
 module.exports = router;
